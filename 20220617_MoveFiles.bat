01d02aeb1476256eb61af493d5dc27e8cf0f5ae6
kinkinbeer135ml <52403447+landmaster135@users.noreply.github.com>
2022-06-17 18:16:05 +0900

    Update 20220617_MoveFiles.bat

--- 20220617_MoveFiles.bat
+++ 20220617_MoveFiles.bat
@@ -3,6 +3,7 @@ set destFolder=ScreenshotOfSatisfactory
 set destFolderFull="%userprofile%\Downloads\%destFolder%"
 set srcFolder="%userprofile%\Documents\My Games\FactoryGame\Screenshots"
 echo %destFolder%
+echo %destFolderFull%
 echo %srcFolder%
 :: Moves files
 mkdir %destFolderFull% > NUL 2>&1
