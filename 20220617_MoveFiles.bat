a7cd4844a13bf31f0129ef1a5bf6d3aaa66f9602
kinkinbeer135ml <52403447+landmaster135@users.noreply.github.com>
2022-06-17 18:09:33 +0900

    Update 20220617_MoveFiles.bat

--- 20220617_MoveFiles.bat
+++ 20220617_MoveFiles.bat
@@ -1,11 +1,16 @@
 :: Initializes variables
-set destFolder="%userprofile%\Downloads\ScreenshotOfSatisfactory"
+set destFolder=ScreenshotOfSatisfactory
+set destFolderFull="%userprofile%\Downloads\%destFolder%"
 set srcFolder="%userprofile%\Documents\My Games\FactoryGame\Screenshots"
 echo %destFolder%
 echo %srcFolder%
 :: Moves files
+mkdir %destFolderFull% > NUL 2>&1
 move /-Y %srcFolder%\* %destFolder%
 :: Releases variables
 set destFolder=
+set destFolderFull=
 set srcFolder=
-%destFolder
+%destFolder%
+%destFolderFull%
+%srcfolder%
